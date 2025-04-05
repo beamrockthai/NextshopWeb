e0251d01ce6a2359ee321fc06576f3a8f92e6f75
beamrockthai <beamrookthat@gmail.com>
2025-04-05 15:57:51 +0700

    Deploy

--- FontendNextShop/src/api/marketApi.js
+++ FontendNextShop/src/api/marketApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/Market";
+// const API_URL = "http://localhost:3001/api/Market";
+
+const API_URL = `${import.meta.env.VITE_API_URL}/Market`;
 
 // ดึงเมนูทั้งหมดมา
 export const getAllMarkets = async () => {
--- FontendNextShop/src/api/user.js
+++ FontendNextShop/src/api/user.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api";
+// const API_URL = "http://localhost:3001/api";
+
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 // user.js
 export const getUserProfile = async () => {
