f2adc0521a3e951b7177bec3fc5acb14c8410f7d
beamrockthai <beamrookthat@gmail.com>
2025-04-05 16:09:11 +0700

    deployhe

--- FontendNextShop/src/api/user.js
+++ FontendNextShop/src/api/user.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_URL = "http://localhost:3001/api";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+const API_URL = `${import.meta.env.VITE_API_URL}`;
 
 // user.js
 export const getUserProfile = async () => {
